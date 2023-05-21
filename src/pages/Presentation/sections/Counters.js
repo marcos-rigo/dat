07b8228c7033c788d50b920e0bce037f711e5e5d
Marcos Rigo <marcos.rigo.10@gmail.com>
2023-05-21 20:43:30 -0300

    agregando el proyecto

---
+++ src/pages/Presentation/sections/Counters.js
@@ -0,0 +1,64 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+import DefaultCounterCard from "examples/Cards/CounterCards/DefaultCounterCard";
+
+function Counters() {
+  return (
+    <MKBox component="section" py={3}>
+      <Container>
+        <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={100}
+              suffix="+"
+              title="Talleres de Verano"
+              description="Como cada verano, llevamos a cabo Talleres para la comunidad tucumana."
+            />
+          </Grid>
+          <Grid item xs={12} md={4} display="flex">
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
+            <DefaultCounterCard
+              count={30}
+              suffix="+"
+              title="Proyectos de Impacto Comunitario"
+              description="Procesos de trabajo en territorios de la provincia."
+            />
+            <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <DefaultCounterCard
+              count={27}
+              suffix="+"
+              title="Mesas de Gestión"
+              description="Llevadas adelante por los responsables de la DAT."
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Counters;
---
+++ src/pages/Presentation/sections/data/pagesData.js
@@ -0,0 +1,37 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+const imagesPrefix = "https://raw.githubusercontent.com/marcos-rigo/portfolio/main/img";
+
+export default [
+  {
+    image: `${imagesPrefix}/map-tal.webp`,
+    name: "Talleres de Verano",
+    route:
+      "https://www.google.com/maps/d/u/0/viewer?mid=1EUhBfpjEVbmG0FYlcJ2Y5qHfMmsoflc&ll=-27.11190657313566%2C-65.34427580000002&z=8",
+  },
+  {
+    image: `${imagesPrefix}/map-pic.webp`,
+    name: "Proyectos de Impacto Comunitario",
+    route:
+      "https://www.google.com/maps/d/u/0/viewer?mid=1oVJBOpLgPMQvMqA8IOZRZXIwxQKT6lA&ll=-27.009601009720615%2C-65.37125830948048&z=10",
+  },
+  {
+    image: `${imagesPrefix}/map-mes.webp`,
+    name: "Mesas de Gestión",
+    route:
+      "https://www.google.com/maps/d/u/0/viewer?mid=1HvUgFHaPo0eesc8Jb0b536StICtdcJ0&ll=-26.822522421369356%2C-65.47320095322563&z=9",
+  },
+];
